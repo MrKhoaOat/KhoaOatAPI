058c9d190bf0f67a4c9aba05ca34e7ffc1ae03a7
MrKhoaOat 6452300113 <tanakornprarom@gmail.com>
2024-02-18 19:47:04 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // เส้นทางสำหรับ API ตัวอย่าง
-app.get('/api/data', (req, res) => {
+app.get('/', (req, res) => {
   // ส่งข้อมูล JSON กลับไปยังผู้ใช้
   res.json({ message: 'This is sample data from the API' });
 });
